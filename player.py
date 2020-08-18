af02ad04559b73844ded3ba3a74224da163fa2e0
Shin'ichiro SUZUKI <shin@sforzando.co.jp>
2020-08-19 00:20:30 +0900

    :wrench: Fix H306

--- player.py
+++ player.py
@@ -1,7 +1,7 @@
-import random
 from abc import ABCMeta
 from abc import abstractmethod
 from dataclasses import dataclass
+from random import choice
 
 from hand import Hand
 
@@ -13,4 +13,4 @@ class Player(metaclass=ABCMeta):
     @abstractmethod
     def next_hand(self):
         hands = [x for x in Hand]
-        return random.choice(hands)
+        return choice(hands)
