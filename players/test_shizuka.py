bf2410dec47bb05f1a2282be4ceea2f74fd784bc
Shin'ichiro SUZUKI <shin@sforzando.co.jp>
2020-08-20 22:43:20 +0900

    :+1: Revise Shizuka

--- players/test_shizuka.py
+++ players/test_shizuka.py
@@ -17,9 +17,9 @@ def test_hands(trials: int = 10000, tolerance: float = 0.1):
     print(f"G: {num_g} → {num_g / trials * 100: .2f}%")
     print(f"C: {num_c} → {num_c / trials * 100: .2f}%")
     print(f"P: {num_p} → {num_p / trials * 100: .2f}%")
-    assert isclose(trials / 3, len([h for h in history if h is Hand.G]), rel_tol=tolerance)
-    assert isclose(trials / 3, len([h for h in history if h is Hand.C]), rel_tol=tolerance)
-    assert isclose(trials / 3, len([h for h in history if h is Hand.P]), rel_tol=tolerance)
+    assert isclose(trials / 3, num_g, rel_tol=tolerance)
+    assert isclose(trials / 3, num_c, rel_tol=tolerance)
+    assert isclose(trials / 3, num_p, rel_tol=tolerance)
 
 
 if __name__ == "__main__":
